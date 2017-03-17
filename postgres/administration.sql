64dd6a141a23bc17e0a568ca99f7de16f50f8c2e
Rafael Mariotti <rmariotti@bionexo.com>
2017-03-17 16:10:24 -0300

    adding query to search for locks

---
+++ postgres/administration.sql
@@ -0,0 +1,17 @@
+# session status
+select datname, usename, application_name, query_start,
+date_part('second', age(now(),query_start)), waiting,
+state, query
+from pg_stat_activity
+where state !='idle';
+
+# check queries which are locking tables
+SELECT blockeda.pid AS blocked_pid, blockeda.query as blocked_query,
+  blockinga.pid AS blocking_pid, blockinga.query as blocking_query
+FROM pg_catalog.pg_locks blockedl
+JOIN pg_stat_activity blockeda ON blockedl.pid = blockeda.pid
+JOIN pg_catalog.pg_locks blockingl ON(blockingl.transactionid=blockedl.transactionid
+  AND blockedl.pid != blockingl.pid)
+JOIN pg_stat_activity blockinga ON blockingl.pid = blockinga.pid
+WHERE NOT blockedl.granted;
+
